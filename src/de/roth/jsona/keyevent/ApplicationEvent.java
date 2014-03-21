7b3a8d00e2bb82009dcff74255329ed57c79622a
frankred <frankred@web.de>
2014-03-21 09:27:01 +0100

    New events added

--- src/de/roth/jsona/keyevent/ApplicationEvent.java
+++ src/de/roth/jsona/keyevent/ApplicationEvent.java
@@ -8,6 +8,7 @@ import com.google.gson.annotations.Expose;
  *
  */
 public enum ApplicationEvent {
-	@Expose PLAYER_NEXT, @Expose PLAYER_PREVIOUS, @Expose PLAYER_PLAY_PAUSE, @Expose PLAYER_VOLUME_UP, @Expose PLAYER_VOLUME_DOWN, @Expose PLAYER_MUTE_UNMUTE, 
+	@Expose PLAYER_NEXT, @Expose PLAYER_PREVIOUS, @Expose PLAYER_PLAY_PAUSE, @Expose PLAYER_VOLUME_UP, @Expose PLAYER_VOLUME_DOWN, @Expose PLAYER_MUTE_UNMUTE,
+	@Expose PLAYER_TIME_UP, @Expose PLAYER_TIME_DOWN, 
 	@Expose VIEW_SEARCH_FOR, @Expose VIEW_HIDE_SHOW
 }
